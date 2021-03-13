e7e1453b0c729e8eb84f5f1135bb3c4791ea3b74
meklenbpo <yk@kantynent.com>
2021-03-13 20:12:23 +0300

    Added an installer

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+pochtaru setuptools installer.
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pochtaru',
+    version='0.0.1',
+    description='Get official postcode information from pochta.ru',
+    packages=find_packages()
+)
